0ff1a387258a440d8d899fa98a58591599463c33
Eduardo Fiuza Cruz <eduardofiuza6@hotmail.com>
2022-04-20 01:57:13 -0300

    Delete IBD directory

--- IBD/App/backend/src/db/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-const config = require("../config/index");
-const Connection = mysql.createConnection(config.mysql);
-
-Connection.connect(error => {
-  if (error) console.log(error);
-});
-
-module.exports = Connection;
--- IBD/App/frontend/src/main/reducers.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-const rootReducer = combineReducers({
-  app: () => ({})
-});
-
-export default rootReducer;
